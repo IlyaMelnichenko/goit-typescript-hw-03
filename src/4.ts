137908110cf279217892e39566433b706faf760b
IlyaMelnichenko <124725419+IlyaMelnichenko@users.noreply.github.com>
2023-10-20 22:46:23 +0300

    a

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,63 @@
-const key = new Key();
-
-const house = new MyHouse(key);
-const person = new Person(key);
-
-house.openDoor(person.getKey());
-
-house.comeIn(person);
-
-
-export {};
+class Key {
+    private signature: number;
+  
+    constructor() {
+      this.signature = Math.random();
+    }
+  
+    getSignature(): number {
+      return this.signature;
+    }
+  }
+  
+ 
+    class Person {
+    constructor(private key: Key) {
+      this.key = key;
+    }
+  
+    getKey(): Key {
+      return this.key;
+    }
+  }
+    abstract class House {
+    protected door: boolean = false;
+    protected tenants: Person[] = [];
+  
+    constructor(protected key: Key) {
+      this.key = key;
+    }
+  
+    abstract openDoor(key: Key): void;
+  
+    comeIn(person: Person): void {
+      if (this.door) {
+        this.tenants.push(person);
+        console.log("You`re in!");
+      } else {
+        console.log("Oops, bad key!");
+      }
+    }
+  }
+  
+    class MyHouse extends House {
+    openDoor(key: Key): void {
+      if (key.getSignature() === this.key.getSignature()) {
+        this.door = true;
+        console.log("Welcome");
+      } else {
+        console.log("Wrong key, go away!!!");
+      }
+    }
+  }
+  
+  const key = new Key();
+  
+  const house = new MyHouse(key);
+  const person = new Person(key);
+  
+  house.openDoor(person.getKey());
+  
+  house.comeIn(person);
+  
+  export {};
